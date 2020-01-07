b20980e292322e4ae779434a1163c7aabacb9e9a
pugongyingh <pgyhh@sina.cn>
2020-01-07 11:46:07 +0800

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,47 +1,31 @@
 
-exports.handler = (event, context, callback) => {
- 
-    var pg = require('pg');
-    //or native libpq bindings
-   // var pg = require('pg').native
 
-    //Can be found in the Details page
-    var conString = "postgres://juicmaka:okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS@arjuna.db.elephantsql.com:5432/juicmaka"
-    var client = new pg.Client(conString);
+const { Client } = require('pg')
 
+exports.handler = async (event) => {
 
+    const connectionString = 'postgres://juicmaka:okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS@arjuna.db.elephantsql.com:5432/juicmaka'
 
+    const client = new Client({
+        connectionString: connectionString
+    })
 
-  client.connect();
-  var query = client.query("SELECT ip_address FROM log_visits");
-  query.then(r => {
-    callback(null, {
-      statusCode: 200,
-      body: "888",
-    });
-  });
- // query.catch(r => console.log("HERE"));
-  //var connection = {
-    //host : 'testdb.cmzabivlkufu.us-east-2.rds.amazonaws.com',
-    //user : 'derek',
-    //password : 'Aa89snj12',
-    //database : 'testdb',
-  //};
+    await client.connect()
 
-  //try {
-    //var client = new pg.Client(connection);
-    //client.connect();
-    //var query = await client.query("SELECT id FROM users;");
+    const res = await client.query('SELECT ip_address FROM log_visits')
 
-    //query.then(r => console.log(r));
-    //query.catch(r => console.log(r));
-    //client.end();
-    //console.log("xs")
-  //}
-  //catch (err) {
-    //console.log("x", err)
-  //}
+    // { pk: '123' }
+    console.log(res.rows[0])
+
+    await client.end()
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(res.rows[0]),
+    };
+
+    return response;
+
 }
-
 
 
