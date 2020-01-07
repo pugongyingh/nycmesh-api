67a680103855cf754b0b14a956d55299209c4752
pugongyingh <pgyhh@sina.cn>
2020-01-07 10:59:28 +0800

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,21 +1,38 @@
-const { Pool } = require('pg');
-
-const con = new Pool({
-  connectionString: 	`postgres://juicmaka:okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS@arjuna.db.elephantsql.com:5432/juicmaka`,
-  ssl: true,
-});
 
 
-
-exports.handler = function(event, context, callback) {
-  var client = new pg.Client(con);
-  client.connect();
-  var query = client.query("SELECT ip_address FROM log_visits");
-  query.then(r => {
+exports.handler = function (event, context, callback) {
     callback(null, {
-      statusCode: 200,
-      body: JSON.stringify(r.rows[0]),
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, x-Requested-Width, Content-Type, Accept'
+        },
+        body: createReturn()
     });
-  });
+}
 
+function createReturn() {
+    //return '{"msg": "Hello visitor from the outer space!"}'
+
+    //var pg = require('pg');
+    //or native libpq bindings
+    var pg = require('pg').native
+
+    //Can be found in the Details page
+    var conString = "postgres://juicmaka:okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS@arjuna.db.elephantsql.com:5432/juicmaka"
+    var client = new pg.Client(conString);
+    client.connect(function (err) {
+        if (err) {
+            return console.error('could not connect to postgres', err);
+        }
+        client.query('SELECT ip_address FROM log_visits', function (err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            var output = JSON.stringify(result.rows[0]);
+            // >> output: 2018-08-23T14:02:57.117Z
+            client.end();
+            return `{"msg": "Current Time: ${ output }"}`;
+        });
+    });
 }
