f2e798b315396ed1a91e688eaefe323420c4d73f
pugongyingh <pgyhh@sina.cn>
2020-01-07 11:07:21 +0800

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -3,10 +3,6 @@
 exports.handler = function (event, context, callback) {
     callback(null, {
         statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Origin, x-Requested-Width, Content-Type, Accept'
-        },
         body: createReturn()
     });
 }
