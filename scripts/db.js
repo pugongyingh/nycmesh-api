e6063d3e4f1cc55567ca1cf7070b4747b4e61bdb
pugongyingh <pgyhh@sina.cn>
2020-01-07 10:30:29 +0800

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -1,51 +1,41 @@
-const { Pool } = require("pg");
+exports.handler = (event, context, callback) => {
+  var pg = require('pg');
+    var connection = {
+      host : 'arjuna.db.elephantsql.com',
+      port: 5432,
+      user : 'juicmaka',
+      password : 'okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS',
+      database : 'juicmaka',
+    };
 
-let pgPool;
+  var client = new pg.Client(connection);
+  client.connect();
+  var query = client.query("SELECT ip_address FROM log_visits;");
+  query.then(r => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(r.rows[0]),
+    });
+  });
+  query.catch(r => console.log("HERE"));
+  //var connection = {
+    //host : 'testdb.cmzabivlkufu.us-east-2.rds.amazonaws.com',
+    //user : 'derek',
+    //password : 'Aa89snj12',
+    //database : 'testdb',
+  //};
 
-async function createPool() {
-	pgPool = new Pool({
-		host: process.env.DB_HOST,
-		database: process.env.DB_NAME,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASS,
-		port: process.env.DB_PORT,
-		ssl: {
-			mode: "require"
-		}
-	});
+  //try {
+    //var client = new pg.Client(connection);
+    //client.connect();
+    //var query = await client.query("SELECT id FROM users;");
+
+    //query.then(r => console.log(r));
+    //query.catch(r => console.log(r));
+    //client.end();
+    //console.log("xs")
+  //}
+  //catch (err) {
+    //console.log("x", err)
+  //}
 }
-
-async function performQuery(text, values) {
-	if (!pgPool) await createPool();
-	const client = await pgPool.connect();
-	const result = await client.query(text, values);
-	client.release();
-	return result.rows;
-}
-
-async function insertBulk(tableName, valueNames, items, valueExtractor) {
-	const commandText = `INSERT INTO ${tableName}`;
-	const valueNameText = `(${valueNames.join(", ")})`; // (id, name, email)
-	let queryText = `${commandText} ${valueNameText} VALUES `;
-
-	const queryValues = [];
-	for (let i = 0; i < items.length; i++) {
-		const values = valueExtractor(items[i]);
-
-		// TODO: This is pretty hacky.. should use named parameters instead
-		// "($1, $2, $3, $4, $5, $6)"
-		const oneToN = Array.from(Array(values.length), (e, i) => i + 1);
-		const offset = queryValues.length;
-		const indexVars = oneToN.map(i => `$${offset + i}`).join(", ");
-		const indexVarsText = `(${indexVars})`;
-
-		queryText += indexVarsText;
-		if (i < items.length - 1) queryText += ", ";
-
-		queryValues.push(...values);
-	}
-
-	return performQuery(queryText, queryValues);
-}
-
-module.exports = { performQuery, insertBulk };
