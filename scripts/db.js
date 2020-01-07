869e94c080eef9e5922af1e020030fdb7615becc
pugongyingh <pgyhh@sina.cn>
2020-01-07 10:52:55 +0800

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -5,36 +5,17 @@ const con = new Pool({
   ssl: true,
 });
 
-const query = (sql, params) => con.connect()
-  .then(client => client.query(sql, params)
-    .then((res) => {
-      client.release();
-      return Promise.resolve(res);
-    })
-    .catch((err) => {
-      client.release();
-      return Promise.reject(err);
-    }));
 
-const insertVisits = async (event) => {
-  const sqlV = `SELECT ip_address FROM log_visits`;
-  return await query(sqlV);
-};
 
 exports.handler = function(event, context, callback) {
-  //console.log(event);
-  console.log('user-agent', event.headers['user-agent']);
-  console.log('x-forwarded-for', event.headers['x-forwarded-for']);
-  console.log(context);
-  //const {identity, user} = context.clientContext;
-  insertVisits(event)
-  .then(res => {
-    console.log(res.rows[0]);
+  var client = new pg.Client(con);
+  client.connect();
+  var query = client.query("SELECT ip_address FROM log_visits");
+  query.then(r => {
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify({ visits: "res.rows[0]" }),
+      body: JSON.stringify(r.rows[0]),
     });
   });
-  
 
 }
