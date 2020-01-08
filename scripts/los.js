479971ae791ba16f805854c69e34d4b8790d1909
pugongyingh <pgyhh@sina.cn>
2020-01-08 20:31:27 +0800

    Update los.js

--- scripts/los.js
+++ scripts/los.js
@@ -17,7 +17,14 @@ exports.handler = async (event) => {
 
     await client.end()
     var n = JSON.stringify(res.rows);
-    
+     if (n!=[]) {
+               if (res.rows[0].xym == 12345678) {
+                    n = "ok"
+                } else {
+                    n = "err"
+                }
+
+    }
 
 
     const response = {
