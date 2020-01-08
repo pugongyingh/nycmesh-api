67aa4e35f82809cb639830c5899c32196d0eda7d
pugongyingh <pgyhh@sina.cn>
2020-01-08 09:25:39 +0800

    Update los.js

--- scripts/los.js
+++ scripts/los.js
@@ -1,44 +1,27 @@
- var pg = require('pg');
-var conString =  'postgres://juicmaka:okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS@arjuna.db.elephantsql.com:5432/juicmaka'
-//exports.handler = function(event, context, callback) {
+const { Client } = require('pg')
+
 exports.handler = async (event) => {
 
-//or native libpq bindings
-//var pg = require('pg').native
-  var response ={
+    const connectionString = 'postgres://juicmaka:okUGxNKWk6CtRezIOHLBHxPbYiGMiQcS@arjuna.db.elephantsql.com:5432/juicmaka'
+
+    const client = new Client({
+        connectionString: connectionString
+    })
+
+    await client.connect()
+
+    const res = await client.query('SELECT  * FROM  behaviour_sample8')
+
+    // { pk: '123' }
+    console.log(res.rows[2])
+
+    await client.end()
+
+    const response = {
         statusCode: 200,
-        body: "999"
+        body: JSON.stringify(res),
     };
 
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-   response ={
-        statusCode: 200,
-        body: "999888"
-    };
-  }
-  client.query('SELECT  * FROM  behaviour_sample8', function(err, result) {
-    if(err) {
-        response = {
-        statusCode: 200,
-        body: "noo"
-    };
-    }
-    else {
- response = {
-        statusCode: 200,
-        body: JSON.stringify(result),
-    };
-                    }
+    return response;
 
-   //console.log(result.rows[0].theTime);
-    // >> output: 2018-08-23T14:02:57.117Z
-    client.end();
-  });
-});
- 
- 
- 
- return response;
 }
