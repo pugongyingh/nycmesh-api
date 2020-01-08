88533502ab28f2f4720edcf542a224bb052eec04
pugongyingh <pgyhh@sina.cn>
2020-01-08 17:04:31 +0800

    Update los.js

--- scripts/los.js
+++ scripts/los.js
@@ -10,39 +10,18 @@ exports.handler = async (event) => {
 
     await client.connect()
 
-   // const res = await client.query("DELETE FROM behaviour_sample8 WHERE notes = 'Tom66'")
- var jg ="55";
-    
-           var qString = "DELETE FROM behaviour_sample8 WHERE notes = 'Tomm'";
-        client.query(qString, function(err, result) {
-          if(err) {
-            client.end();
-            //  jg="err77";
-                const response = {
+    const res = await client.query('DELETE FROM behaviour_sample8 WHERE notes = 'Tom' RETURNING notes')
+
+    // { pk: '123' }
+    //console.log(res.rows[2])
+
+    await client.end()
+
+    const response = {
         statusCode: 200,
-        body: "err77"
+        body: res,
     };
 
     return response;
-          }
-     
-          //jg="Delete Successfully";
-             const responsee = {
-        statusCode: 200,
-        body: "Delete Successfully"
-    };
-
-    return responsee;
-          client.end();
-        }); 
-    
-    
-    // { pk: '123' }
-   // console.log(res.rows[2])
-
-   // await client.end()
-
-    
-
 
 }
