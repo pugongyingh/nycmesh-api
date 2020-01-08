f42e5c879a9e9d71f984c84442a5bdbfef136d61
pugongyingh <pgyhh@sina.cn>
2020-01-08 11:59:25 +0800

    Update los.js

--- scripts/los.js
+++ scripts/los.js
@@ -10,17 +10,32 @@ exports.handler = async (event) => {
 
     await client.connect()
 
-    const res = await client.query("DELETE FROM behaviour_sample8 WHERE notes = 'Tom66'")
-
+   // const res = await client.query("DELETE FROM behaviour_sample8 WHERE notes = 'Tom66'")
+ var jg ="55";
+    
+           var qString = "DELETE FROM behaviour_sample8 WHERE notes = 'Tomm'";
+        client.query(qString, function(err, result) {
+          if(err) {
+            client.end();
+              jg="err77";
+            
+          }
+     
+          jg="Delete Successfully";
+         
+          client.end();
+        }); 
+    
+    
     // { pk: '123' }
    // console.log(res.rows[2])
 
-    await client.end()
+   // await client.end()
 
     
     const response = {
         statusCode: 200,
-        body: res.toString()
+        body: jg
     };
 
     return response;
